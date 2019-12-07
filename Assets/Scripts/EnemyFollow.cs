e349c1529c7c44f02c922227157df052aa786f19
Stormjargames <stormjargames@gmail.com>
2019-12-07 12:10:52 +0000

    JamesUseThisToPlay
    James builds a walkabout with post processing + Aura

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    private NavMeshAgent EnemyOne;
+
+    public GameObject Player;
+
+    public float EnemyOneDistanceRun = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemyOne = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        // Run towards player
+
+        if (distance < EnemyOneDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            EnemyOne.SetDestination(newPos);
+        }
+    }
+}
